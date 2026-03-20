cf76f148c9d5be231dd6a1315ec66941155aba6d
Anastasiia Lets <alets1@sheffield.ac.uk>
2026-03-20 13:06:31 +0000

    feat: added a vertical slider

--- app/src/main/java/com/example/echo/MainActivity.kt
+++ app/src/main/java/com/example/echo/MainActivity.kt
@@ -4,29 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowColumn
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.dp
 import com.example.echo.ui.theme.EchoTheme
 
 class MainActivity : ComponentActivity() {
@@ -59,8 +70,37 @@ enum class Destination(
 }
 
 @Composable
-fun Sliders(){
-    //There is a vertical slider directly from material design
+fun VerticalSlider(modifier: Modifier = Modifier){
+    var sliderPosition by remember { mutableFloatStateOf(0f) }
+    Column(
+        modifier
+            .padding(0.dp,150.dp),
+        //verticalArrangement = Arrangement.Center,
+        //horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        //Spacer(Modifier.height(200.dp))
+        Slider(
+            value = sliderPosition,
+            onValueChange = {sliderPosition = it},
+            modifier = Modifier
+                .height(44.dp)
+                .width(300.dp)
+                //.padding(150.dp,20.dp)
+                .rotate(-90f)
+        )
+    }
+}
+
+@Composable
+fun Sliders(modifier: Modifier = Modifier){
+    Row(
+        modifier.padding(25.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        VerticalSlider()
+        VerticalSlider()
+    }
+
 
 }
 
@@ -104,7 +144,7 @@ fun NavigationBarScaffold(){
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier){
-    Sliders()
+    Sliders(modifier)
     Charge()
     FavOptions()
 }
