8252f5bcea4951927208f06704b48fa2e1c97766
Anastasiia Lets <alets1@sheffield.ac.uk>
2026-03-20 13:45:28 +0000

    fix: can run screen

--- app/src/main/java/com/example/echo/MainActivity.kt
+++ app/src/main/java/com/example/echo/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.echo.ui.screens.HelpAndTroubleshooting
 import com.example.echo.ui.theme.EchoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             EchoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HelpAndTroubleshooting(Modifier.padding(innerPadding))
                 }
             }
         }
